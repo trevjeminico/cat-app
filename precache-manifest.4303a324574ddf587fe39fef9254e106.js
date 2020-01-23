90d6ecd1bb1dfaf2cadfd1a6388b489bd029108a
tjeminico-c <trevjeminico@gmail.com>
2020-01-23 18:38:46 +0800

    Updates

---
+++ precache-manifest.4303a324574ddf587fe39fef9254e106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c41c1eb6cf450aaf39a5412ff16bb83",
+    "url": "/cat-app/index.html"
+  },
+  {
+    "revision": "50ec1191c3b1993c90f3",
+    "url": "/cat-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4376f53054ad2af80985",
+    "url": "/cat-app/static/js/2.c1c78cac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cat-app/static/js/2.c1c78cac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50ec1191c3b1993c90f3",
+    "url": "/cat-app/static/js/main.ba73660c.chunk.js"
+  },
+  {
+    "revision": "53482149e5dd766b5df6",
+    "url": "/cat-app/static/js/runtime-main.d2166e15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cat-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
